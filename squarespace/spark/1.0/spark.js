6f1d314855dcd5e1010cfdf40fe0c297383ed2e3
pixelnorm <pixelnorm.com@gmail.com>
2026-06-09 16:06:41 +0100

    Add files via upload

---
+++ squarespace/spark/1.0/spark.js
@@ -0,0 +1,99 @@
+/*
+=================================
+PixelNorm SPARK
+Version 1.0
+Theme Controller
+=================================
+*/
+
+
+document.addEventListener("DOMContentLoaded", function(){
+
+
+const savedTheme = localStorage.getItem("spark-theme");
+
+
+if(savedTheme === "dark"){
+ document.documentElement.classList.add("spark-dark");
+}
+
+
+/* create toggle */
+
+const btn=document.createElement("button");
+
+btn.className="spark-theme-toggle";
+
+btn.innerHTML =
+document.documentElement.classList.contains("spark-dark")
+? "☀︎"
+: "◐";
+
+
+
+/* desktop placement */
+
+let desktopTarget =
+document.querySelector(".header-actions");
+
+
+if(desktopTarget){
+ desktopTarget.prepend(btn);
+}
+
+
+/* mobile placement */
+
+let mobileTarget =
+document.querySelector(".header-menu-actions");
+
+
+if(mobileTarget){
+
+ let mobileBtn = btn.cloneNode(true);
+
+ mobileTarget.prepend(mobileBtn);
+
+ mobileBtn.onclick=function(){
+  toggleSparkTheme();
+ };
+
+}
+
+
+/* click */
+
+btn.onclick=function(){
+
+ toggleSparkTheme();
+
+};
+
+
+
+function toggleSparkTheme(){
+
+ document.documentElement.classList.toggle("spark-dark");
+
+
+ let dark =
+ document.documentElement.classList.contains("spark-dark");
+
+
+ localStorage.setItem(
+ "spark-theme",
+ dark ? "dark" : "light"
+ );
+
+
+ document.querySelectorAll(".spark-theme-toggle")
+ .forEach(function(item){
+
+ item.innerHTML = dark ? "☀︎" : "◐";
+
+ });
+
+}
+
+
+});
